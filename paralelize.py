682c7deab1a782145ce7d29b15ff1bdd7b28603e
joao paulo <pool.martin@gmail.com>
2019-05-04 15:38:41 -0300

    order process

--- paralelize.py
+++ paralelize.py
@@ -2,6 +2,7 @@ from joblib import Parallel, delayed
 
 import argparse, os
 from subprocess import call
+os.environ['JOBLIB_TEMP_FOLDER'] = "~/tmp"
 
 def load_args():
     ap = argparse.ArgumentParser(description='Paralelize Saliency maps process.')
@@ -23,11 +24,39 @@ def extractSaliencyMaps(args, video):
     print('\n', command)
     call(command, shell=True)
 
+def video_process_finished(args, videos_len, video):
+    nb_frames_processed = len(os.listdir(os.path.join(args.output_path, video)))
+    if nb_frames_processed != videos_len(video):
+        return False
+    else:
+        return True
+
+def get_lens(args, videos):
+    video_len = {}
+    for video in videos:
+        video_len[video] = len(os.listdir(os.path.join(args.dataset_dir, "frames", video)))
+    return video_len
+
 def main():
     args = load_args()
 
-    videos = os.listdir(os.path.join(args.dataset_dir, "frames")) 
-    Parallel(n_jobs=3)(delayed(extractSaliencyMaps)(args, video) for video in videos)
+    videos_to_process = os.listdir(os.path.join(args.dataset_dir, "frames"))
+    videos_processed = os.listdir(os.path.join(args.output_path))
+
+    videos_len = get_lens(args, videos_to_process)
+
+    for video in videos_processed:
+        if (video_process_finished(args, videos_len, video)):
+            videos_to_process.remove(video)
+            del videos_len[video]
+
+    ordered_videos_len = sorted(videos_len.items(), key=lambda kv: kv[1])
+    sorted_videos_dict = OrderedDict(ordered_videos_len)
+
+    for i, (video, len_value) in enumerate(sorted_videos_dict.items()):
+        extractSaliencyMaps(args, video)
+
+#    Parallel(n_jobs=3)(delayed(extractSaliencyMaps)(args, video) for video in videos)
 
 if __name__ == '__main__':
     main()
