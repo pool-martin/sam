ccbc4c28bfa29885ed6727b27fa80fa03e240766
joao paulo <pool.martin@gmail.com>
2019-05-04 13:08:10 -0300

    fix

--- paralelize.py
+++ paralelize.py
@@ -12,14 +12,14 @@ def load_args():
     ap.add_argument('-o', '--output-path',
                                     dest='output_path',
                                     help='path to output the extracted frames.',
-                                    type=str, required=False, default='/DL/2kporn/frames/')
+                                    type=str, required=False, default='/DL/2kporn/saliency_frames/')
     args = ap.parse_args()
     print(args)
     return args
 
 def extractSaliencyMaps(args, video):
 
-    command = "python main.py test {}".format(os.path.join(args.dataset_dir, "frames", video))
+    command = "python main.py test {}/ {}/".format(os.path.join(args.dataset_dir, "frames", video), args.output_path)
     print('\n', command)
     call(command, shell=True)
 
@@ -32,7 +32,7 @@ def extract(args, all_set):
         for i in range(1, 1001):
             video = 'v{}{}'.format(class_type, str(i).zfill(6))
 
-    Parallel(n_jobs=20)(delayed(extractSaliencyMaps)(args, video) for video in video_frames.keys())
+    Parallel(n_jobs=3)(delayed(extractSaliencyMaps)(args, video) for video in video_frames.keys())
 
     
 
